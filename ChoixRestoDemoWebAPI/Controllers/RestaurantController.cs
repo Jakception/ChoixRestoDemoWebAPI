f72eda3576fa4bd59351426a78978677dd5cd10d
Jakception <gbeulaygue@gmail.com>
2017-09-02 02:16:25 +0200

    Fin Web API

---
+++ ChoixRestoDemoWebAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,39 @@
+﻿using ChoixRestoDemoWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChoixRestoDemoWebAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        public IEnumerable<Resto> GetTousLesRestos()
+        {
+            return new Restos().ChargerRestaurants();
+        }
+
+        public IHttpActionResult GetResto(int id)
+        {
+            Resto resto = new Restos().ChargerRestaurants().FirstOrDefault(r => r.Id == id);
+            if (resto == null)
+            {
+                return NotFound();
+            }
+            return Ok(resto);
+        }
+
+        [HttpGet]
+        public IHttpActionResult TrouveResto(int id)
+        {
+            Resto resto = new Restos().ChargerRestaurants().FirstOrDefault(r => r.Id == id);
+            if (resto == null)
+            {
+                return NotFound();
+            }
+            return Ok(resto);
+        }
+    }
+}
---
+++ ChoixRestoDemoWebAPI/Models/Resto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixRestoDemoWebAPI.Models
+{
+    public class Resto
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+
+    public class Restos
+    {
+        public List<Resto> ChargerRestaurants()
+        {
+            return new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinière"},
+                new Resto { Id = 2, Nom = "Resto pinambour"},
+                new Resto { Id = 3, Nom = "Resto ride"},
+            };
+        }
+    }
+}
